b8ee6ee1775b60cc558a71022e34d09782e28036
Christian Rebelsky <christian.rebelsky@gmail.com>
2018-03-07 22:48:03 +0100

    initial commit

---
+++ src/client/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,14 @@
+//Simple error page 
+import React, { Component } from 'react';
+import {MainLayout} from '../../components/';
+
+
+export default class ErrorPage extends Component {
+  render() {
+    return (
+      <MainLayout>
+        <h2>Error: page not found</h2>
+      </MainLayout>
+    );
+  }
+}
